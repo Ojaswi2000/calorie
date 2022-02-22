577713156f362c569ba8e0c8f30675ab4f5b93f7
Ojaswi <ojaswiawasthi2025@gmail.com>
2022-02-22 20:44:07 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css';
 
 const App=() => {
+  const [item,setItem]=useState(null);
+  const [amount,setAmount]=useState(null);
   return (
     <div className='App'>
+      <div className='inputs'>
+      <input type="text" placeholder='Item name' value={item} 
+      onChange={(e)=>{
+        setItem(e.target.value);
+      }}
+      />
+
+      <input type="text" placeholder='Item name' value={amount} 
+      onChange={(e)=>{
+        setAmount(e.target.value);
+      }}
+      />
+
+      </div>
+      
+      <div>
+      <button >Add Item</button>
+      </div>
+      
+      
       
     </div>
   )
