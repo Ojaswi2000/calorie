9f2ab7341fa040165dbbed42351788329c104b36
Ojaswi <ojaswiawasthi2025@gmail.com>
2022-02-23 00:49:33 +0530

    messed up styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../App.css';
 
 const Card=({item,amount})=>{
   return (
     <div className='container'>
-      
+      <h2>{item}</h2>
+      <p>You have consumed {amount} calories</p>
+      <div>
+        <button className='delete'>Delete</button>
+        <button className='edit'>Edit</button>
+      </div>
     </div>
   )
 }
